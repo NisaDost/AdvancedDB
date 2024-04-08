5ae34f7a526d1d20a2a4850cff2cc64b846be162
Nisa Dost <200706004@st.maltepe.edu.tr>
2024-04-08 19:36:45 +0300

    DEADLOCK COUNTER ERROR
    //DEADLOCK COUNTER WORKS WELL ON SERIALIZABLE LEVEL BUT HAS BUGS ON OTHER LEVELS

--- AdvancedDB_Form/Form1.Designer.cs
+++ AdvancedDB_Form/Form1.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace AdvancedDB_Form
+﻿
+namespace AdvancedDB_Form
 {
     partial class Form1
     {
@@ -31,23 +32,33 @@
             label1 = new Label();
             button1 = new Button();
             comboBox1 = new ComboBox();
+            numericUpDown1 = new NumericUpDown();
+            label2 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            numericUpDown2 = new NumericUpDown();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(124, 46);
+            label1.Location = new Point(187, 25);
             label1.Name = "label1";
-            label1.Size = new Size(300, 20);
+            label1.Size = new Size(140, 20);
             label1.TabIndex = 0;
-            label1.Text = "Select a read status and start the simulation.";
+            label1.Text = "Select a read status.";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
             label1.Click += label1_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(178, 212);
+            button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button1.Location = new Point(104, 231);
             button1.Name = "button1";
-            button1.Size = new Size(176, 29);
+            button1.Size = new Size(179, 54);
             button1.TabIndex = 1;
             button1.Text = "Start simulation";
             button1.UseVisualStyleBackColor = true;
@@ -55,28 +66,97 @@
             // 
             // comboBox1
             // 
-            comboBox1.DisplayMember = "text";
             comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "Read Uncommitted", "Read Committed", "Repeatable Read", "Serializable" });
-            comboBox1.Location = new Point(124, 84);
+            comboBox1.Location = new Point(104, 48);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(300, 28);
+            comboBox1.Size = new Size(327, 28);
             comboBox1.TabIndex = 2;
             comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(164, 172);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(71, 27);
+            numericUpDown1.TabIndex = 3;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(104, 174);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Type A";
+            label2.Click += label1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(104, 119);
+            label4.Name = "label4";
+            label4.Size = new Size(327, 20);
+            label4.TabIndex = 0;
+            label4.Text = "Select numbers of users to perform transactions.";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            label4.Click += label1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(301, 174);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 20);
+            label3.TabIndex = 0;
+            label3.Text = "Type B";
+            label3.Click += label1_Click;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(360, 172);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(71, 27);
+            numericUpDown2.TabIndex = 3;
+            numericUpDown2.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(310, 231);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 54);
+            button2.TabIndex = 4;
+            button2.Text = "Quit";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(537, 274);
+            ClientSize = new Size(537, 315);
+            Controls.Add(button2);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
             Controls.Add(comboBox1);
             Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label4);
             Controls.Add(label1);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterParent;
             Text = "Transaction Manager";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
             ResumeLayout(false);
             PerformLayout();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
         }
 
         #endregion
@@ -84,5 +164,11 @@
         private Label label1;
         private Button button1;
         private ComboBox comboBox1;
+        private NumericUpDown numericUpDown1;
+        private Label label2;
+        private Label label4;
+        private Label label3;
+        private NumericUpDown numericUpDown2;
+        private Button button2;
     }
 }
--- AdvancedDB_Form/Form1.cs
+++ AdvancedDB_Form/Form1.cs
@@ -1,25 +1,250 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace AdvancedDB_Form
 {
+    //DEADLOCK COUNTER WORKS WELL ON SERIALIZABLE LEVEL BUT HAS BUGS ON OTHER LEVELS
     public partial class Form1 : Form
     {
+        int transactionNumber = 5; // Set low for testing purposes
         public Form1()
         {
             InitializeComponent();
+
+            comboBox1.Items.AddRange(new object[] { "Read Uncommitted", "Read Committed", "Repeatable Read", "Serializable" });
+            comboBox1.SelectedIndex = 0; // Default Read Uncommitted
         }
 
+        #region Start Simulation Button
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("***\nSimulation started...\n***\n");
+
+            string connectionString = "Data Source=NISA-PC;Initial Catalog=AdventureWorks2022;Integrated Security=True;";
+            int selectedIsolationLevel = comboBox1.SelectedIndex;
+            IsolationLevel isolationLevel = GetIsolationLevel(selectedIsolationLevel);
+
+            int numberOfTypeAUsers = (int)numericUpDown1.Value; // number of type A users
+            int numberOfTypeBUsers = (int)numericUpDown2.Value; // number of type B users
+
+            int deadlockCountA = 0;
+            int deadlockCountB = 0;
+
+            var tasks = new Task[numberOfTypeAUsers + numberOfTypeBUsers];
+
+            var beginTime = DateTime.Now;
+
+            for (int i = 0; i < numberOfTypeAUsers; i++)
+            {
+                tasks[i] = Task.Run(() => TypeAUserTransaction(connectionString, isolationLevel, ref deadlockCountA));
+            }
+
+            for (int i = numberOfTypeAUsers; i < numberOfTypeAUsers + numberOfTypeBUsers; i++)
+            {
+                tasks[i] = Task.Run(() => TypeBUserTransaction(connectionString, isolationLevel, ref deadlockCountB));
+            }
+
+            await Task.WhenAll(tasks);
+
+            var endTime = DateTime.Now;
+            var elapsed = endTime - beginTime;
+
+            MessageBox.Show($"Elapsed time: {elapsed.TotalSeconds} seconds\nDeadlocks (Type A): {deadlockCountA}\nDeadlocks (Type B): {deadlockCountB}", "Simulation Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            Console.WriteLine("\n***\nSimulation complete.\n***\n");
+        }
+        #endregion
+
+        #region Isolation Level
+        private IsolationLevel GetIsolationLevel(int selectedIndex)
+        {
+            switch (selectedIndex)
+            {
+                case 0:
+                    return IsolationLevel.ReadUncommitted;
+                case 1:
+                    return IsolationLevel.ReadCommitted;
+                case 2:
+                    return IsolationLevel.RepeatableRead;
+                case 3:
+                    return IsolationLevel.Serializable;
+                default:
+                    return IsolationLevel.ReadUncommitted; // Default to Read Uncommitted
+            }
+        }
+        #endregion
+
+        #region User Transaction Methods
+        private void TypeAUserTransaction(string connectionString, IsolationLevel isolationLevel, ref int deadlockCountA)
+        {
+            var rand = new Random();
+            var beginTime = DateTime.Now;
+
+            for (int i = 0; i < transactionNumber; i++) // Perform 100 transactions
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (var transaction = connection.BeginTransaction(isolationLevel))
+                    {
+                        try
+                        {
+                            if (rand.NextDouble() < 0.5)
+                                RunUpdateQuery(connection, transaction, "20110101", "20111231");
+                            if (rand.NextDouble() < 0.5)
+                                RunUpdateQuery(connection, transaction, "20120101", "20121231");
+                            if (rand.NextDouble() < 0.5)
+                                RunUpdateQuery(connection, transaction, "20130101", "20131231");
+                            if (rand.NextDouble() < 0.5)
+                                RunUpdateQuery(connection, transaction, "20140101", "20141231");
+                            if (rand.NextDouble() < 0.5)
+                                RunUpdateQuery(connection, transaction, "20150101", "20151231");
+
+                            transaction.Commit();
+                        }
+                        catch (SqlException ex)
+                        {
+                            if (ex.Number == 1205) // Deadlock error number
+                            {
+                                // Deadlock occurred, increment deadlock count for Type A
+                                deadlockCountA++;
+                                Console.WriteLine("Deadlock occurred for Type A. Continuing gracefully.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Transaction failed: " + ex.Message);
+                                transaction.Rollback();
+                            }
+                        }
+                    }
+                }
+            }
+
+            var endTime = DateTime.Now;
+            var elapsed = endTime - beginTime;
+            Console.WriteLine($"Elapsed time for Type A user: {elapsed.TotalSeconds} seconds");
+        }
+
+
+        private void TypeBUserTransaction(string connectionString, IsolationLevel isolationLevel, ref int deadlockCountB)
+        {
+            var rand = new Random();
+            var beginTime = DateTime.Now;
+
+            for (int i = 0; i < transactionNumber; i++) // Perform 100 transactions
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (var transaction = connection.BeginTransaction(isolationLevel))
+                    {
+                        try
+                        {
+                            if (rand.NextDouble() < 0.5)
+                                RunSelectQuery(connection, transaction, "20110101", "20111231");
+                            if (rand.NextDouble() < 0.5)
+                                RunSelectQuery(connection, transaction, "20120101", "20121231");
+                            if (rand.NextDouble() < 0.5)
+                                RunSelectQuery(connection, transaction, "20130101", "20131231");
+                            if (rand.NextDouble() < 0.5)
+                                RunSelectQuery(connection, transaction, "20140101", "20141231");
+                            if (rand.NextDouble() < 0.5)
+                                RunSelectQuery(connection, transaction, "20150101", "20151231");
+
+                            transaction.Commit();
+                        }
+                        catch (SqlException ex)
+                        {
+                            if (ex.Number == 1205) // Deadlock error number
+                            {
+                                // Deadlock occurred, increment deadlock count for Type B
+                                deadlockCountB++;
+                                Console.WriteLine("Deadlock occurred for Type B. Continuing gracefully.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Transaction failed: " + ex.Message);
+                                transaction.Rollback();
+                            }
+                        }
+                    }
+                }
+            }
+
+            var endTime = DateTime.Now;
+            var elapsed = endTime - beginTime;
+
+            Console.WriteLine($"Elapsed time for Type B user: {elapsed.TotalSeconds} seconds");
+        }
+
+        #endregion
+
+        #region SQL Queries
+        static void RunUpdateQuery(SqlConnection connection, SqlTransaction transaction, string beginDate, string endDate)
+        {
+            string updateQuery = @"
+                UPDATE Sales.SalesOrderDetail
+                SET UnitPrice = UnitPrice * 10.0 / 10.0
+                WHERE UnitPrice > 100
+                AND EXISTS (
+                    SELECT * FROM Sales.SalesOrderHeader
+                    WHERE Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID
+                    AND Sales.SalesOrderHeader.OrderDate BETWEEN @BeginDate AND @EndDate
+                    AND Sales.SalesOrderHeader.OnlineOrderFlag = 1
+                )";
+
+            using (var command = new SqlCommand(updateQuery, connection, transaction))
+            {
+                command.Parameters.AddWithValue("@BeginDate", beginDate);
+                command.Parameters.AddWithValue("@EndDate", endDate);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        static void RunSelectQuery(SqlConnection connection, SqlTransaction transaction, string beginDate, string endDate)
+        {
+            string selectQuery = @"
+                SELECT SUM(Sales.SalesOrderDetail.OrderQty)
+                FROM Sales.SalesOrderDetail
+                WHERE UnitPrice > 100
+                AND EXISTS (
+                    SELECT * FROM Sales.SalesOrderHeader
+                    WHERE Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID
+                    AND Sales.SalesOrderHeader.OrderDate BETWEEN @BeginDate AND @EndDate
+                    AND Sales.SalesOrderHeader.OnlineOrderFlag = 1
+                )";
+
+            using (var command = new SqlCommand(selectQuery, connection, transaction))
+            {
+                command.Parameters.AddWithValue("@BeginDate", beginDate);
+                command.Parameters.AddWithValue("@EndDate", endDate);
+                command.ExecuteScalar();
+            }
+        }
+        #endregion
+
+        #region Necessary methods
+        // IDK why but without this methods the program won't run
         private void label1_Click(object sender, EventArgs e)
         {
-
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
         {
-            //this.Close();
+
+        }
+        #endregion
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
