f427df095826a8a0b75c075c75dd2074bcdc3dba
Nisa Dost <200706004@st.maltepe.edu.tr>
2024-04-08 14:27:35 +0300

    intro

---
+++ AdvancedDB_Form/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace AdvancedDB_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(124, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(300, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Select a read status and start the simulation.";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(178, 212);
+            button1.Name = "button1";
+            button1.Size = new Size(176, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Start simulation";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DisplayMember = "text";
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Read Uncommitted", "Read Committed", "Repeatable Read", "Serializable" });
+            comboBox1.Location = new Point(124, 84);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(300, 28);
+            comboBox1.TabIndex = 2;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(537, 274);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Transaction Manager";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private ComboBox comboBox1;
+    }
+}
---
+++ AdvancedDB_Form/Form1.cs
@@ -0,0 +1,25 @@
+namespace AdvancedDB_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+        }
+    }
+}
